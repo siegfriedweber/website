a2464ad57bbf67796756821e0cf6af7f0167f2eb
Siegfried Weber <siegfried_weber@it-dreams.de>
2017-02-07 14:42:27 +0100

    Links to Gulp, Xing, Linked in, GitHub, and Stack Overflow added

--- src/Page.purs
+++ src/Page.purs
@@ -8,7 +8,9 @@ import Prelude
 import Unsafe.Coerce (unsafeCoerce)
 
 import CSS as C
+import CSS.ListStyle.Type as CL
 import CSS.Common as CC
+import CSS.TextAlign as CT
 import CSS.VerticalAlign as CV
 import Halogen as H
 import Halogen.HTML.CSS as HS
@@ -77,10 +79,56 @@ header =
     styleDesc =
         styleFont SourceSansProLight
 
+links :: forall p i. HH.HTML p i
+links =
+    HH.div
+        [ HC.style $ C.backgroundColor $ C.fromInt 0xf1f1f1 ]
+        [ HH.div
+            [ HC.style do
+                 C.width $ C.px 960.0
+                 C.marginLeft CC.auto
+                 C.marginRight CC.auto
+                 C.backgroundColor $ C.fromInt 0x002b36
+                 C.paddingTop $ C.px 24.0
+                 C.paddingBottom $ C.px 24.0
+            ]
+            [ inlineList
+                [ link "Gulp" "gulp.png" "https://www.gulp.de/gulp2/home/profil/siegfriedweber"
+                , link "Xing" "xing.png" "https://www.xing.com/profile/Siegfried_Weber18"
+                , link "Linked in" "linkedin.png" "https://de.linkedin.com/in/siegfriedweber"
+                , link "GitHub" "github.png" "https://github.com/siegfriedweber"
+                , link "Stack Overflow" "stackoverflow.png" "https://stackoverflow.com/users/7312398/siegfried-weber"
+                ]
+            ]
+        ]
+  where
+    inlineList :: forall p' i'. Array (HH.HTML p' i') -> HH.HTML p' i'
+    inlineList = HH.ul
+        [ HC.style do
+            CT.textAlign CT.center
+            CL.listStyleType CC.none
+            C.marginTop C.nil
+            C.marginBottom C.nil
+            C.paddingLeft C.nil
+        ]
+
+    link :: forall p' i'. String -> String -> String -> HH.HTML p' i'
+    link name icon url =
+        HH.li [ HC.style $ C.display C.inlineBlock ]
+            [ HH.a
+                [ HP.href url
+                , HC.style do
+                    C.paddingLeft $ C.px 12.0
+                    C.paddingRight $ C.px 12.0
+                ]
+                [ HH.img [ HP.src $ "images/" <> icon, HP.alt name ] ]
+            ]
+
 website :: forall p i. HH.HTML p i
 website = HH.div_
     [ unsafeCoerce $ HS.stylesheet $ styleBody *> styleFontFaces
     , header
+    , links
     ]
 
 styleBody :: C.CSS
